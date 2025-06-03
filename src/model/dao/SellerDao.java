b35be236b63d0575cb1bc63940c94a3b39e02d9f
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2025-06-03 14:21:10 -0300

    Implementação de Update

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface SellerDao {
     void insert(Seller obj);
-    void update(int id, String name, int quantity, double price);
+    void update(Seller obj);
     void deleteById(int id);
     Seller findById(int id);
     List<Seller> findAll();
